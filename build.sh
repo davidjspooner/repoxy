fa041529081974f11e2f914bf498bf82ba466f14
DavidJSpooner <davidjspooner@gmail.com>
2025-06-08 22:22:36 +0100

    feat: Add .gitignore for dist directory and update build script for environment variable exports

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 export CMD=repoxy
 
 TMP_ENV_FILE=$(mktemp)
-ci-utility git suggest-build-env > "$TMP_ENV_FILE"
+ci-utility git suggest-build-env --command-prefix "export " > "$TMP_ENV_FILE"
 if [[ ! -s "$TMP_ENV_FILE" ]]; then
     echo "Failed to determine build environment"
     rm -f "$TMP_ENV_FILE"
@@ -13,7 +13,7 @@ if [[ ! -s "$TMP_ENV_FILE" ]]; then
 fi
 
 # check that the environment file is valid
-if ! grep -qE '^BUILD_TIME=".+"$' "$TMP_ENV_FILE"; then
+if ! grep -qE '^export BUILD_TIME=".+"$' "$TMP_ENV_FILE"; then
     echo "Invalid environment file format: $TMP_ENV_FILE"
     cat -vet "$TMP_ENV_FILE"
     #rm -f "$TMP_ENV_FILE"
@@ -28,23 +28,25 @@ ci-utility matrix run \
     -d GOOS=linux \
     -d GOARCH=amd64,arm64 \
     -d CGO_ENABLED=0 \
-    -- \
+    << __EOF__
+    set -x
     go build -ldflags="-s -w \
-        -X 'github.com/davidjspooner/go-text-cli/pkg/cmd.BUILD_VERSION=${BUILD_VERSION}' \
-        -X 'github.com/davidjspooner/go-text-cli/pkg/cmd.BUILD_BY=${BUILD_BY}' \
-        -X 'github.com/davidjspooner/go-text-cli/pkg/cmd.BUILD_TIME=${BUILD_TIME}' \
-        -X 'github.com/davidjspooner/go-text-cli/pkg/cmd.BUILD_FROM=${BUILD_FROM}' " \
-        -o 'dist/${CMD}-${GOOS}-${GOARCH}' './cmd/${CMD}'
+        -X 'github.com/davidjspooner/go-text-cli/pkg/cmd.BUILD_VERSION=\${BUILD_VERSION}' \
+        -X 'github.com/davidjspooner/go-text-cli/pkg/cmd.BUILD_BY=\${BUILD_BY}' \
+        -X 'github.com/davidjspooner/go-text-cli/pkg/cmd.BUILD_TIME=\${BUILD_TIME}' \
+        -X 'github.com/davidjspooner/go-text-cli/pkg/cmd.BUILD_FROM=\${BUILD_FROM}' " \
+        -o 'dist/\${CMD}-\${GOOS}-\${GOARCH}' './cmd/\${CMD}'
+__EOF__
 
-# OS=$(uname | tr '[:upper:]' '[:lower:]')
-# ARCH=$(uname -m)
-# case "$ARCH" in
-#     x86_64) ARCH="amd64" ;;
-#     aarch64) ARCH="arm64" ;;
-#     *) echo "Unsupported architecture: ${ARCH}"; exit 1 ;;
-# esac
-# 
-# echo -e "\nCopying binary to /usr/local/bin/${CMD}\n"
-# sudo rm -f /usr/local/bin/${CMD}
-# sudo cp dist/${CMD}-"${OS}"-"${ARCH}" /usr/local/bin/${CMD}
-# /usr/local/bin/${CMD} version
+OS=$(uname | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+case "$ARCH" in
+    x86_64) ARCH="amd64" ;;
+    aarch64) ARCH="arm64" ;;
+    *) echo "Unsupported architecture: ${ARCH}"; exit 1 ;;
+esac
+
+echo -e "\nCopying binary to /usr/local/bin/${CMD}\n"
+sudo rm -f /usr/local/bin/${CMD}
+sudo cp dist/${CMD}-"${OS}"-"${ARCH}" /usr/local/bin/${CMD}
+/usr/local/bin/${CMD} version
